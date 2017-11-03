9d5002a2e75b3b6410f4359c4f8b7dae1d732787
Marco3jp <k_kimu_jr@yahoo.co.jp>
2017-11-03 21:50:52 +0900

    Write comment for transitioning from Each variable to Object

--- testEnv/functionDev/commuTest/readData.js
+++ testEnv/functionDev/commuTest/readData.js
@@ -1,22 +1,55 @@
-var displayID;
-var textPoint;
-var characters;
-var endFlag;
-var printFlag;
-var playingIdolNum;
 var messages;
 var nextMessage;
-var commuEndFlag;
 var commuNameArray = [];
 var commuTextArray = [];
-var qFlag;
-var choiceId=0;
-var commuPoint;
+var characters;
+/*
+    **Parent-child relationship**
+     produceHistory.idolData[id.playingIdol].commu.*
+        - messages (by communication())
+            - nextMessage (by toArray())
+            - commuNameArray (by toArray())
+            - commuTextArray (by toArray())
+                - characters (by textCutter())
+*/
+
 var overOneFunctionValue;
+
+var playingIdolNum;
+var commuPoint;
+var textPoint;
+var choiceId=0;
+var displayID;
+
+var printFlag;
+var commuEndFlag;
+var qFlag;
+var endFlag;
+
+var flag={"displayEnd": ,"printEnd": , "q": ,"commuEnd":,}
+var id={"playingIdol","display":,}
+/*
+    ** Not Finished Change variable. I wrote plans below**
+    *Flag -> flag.*
+        endFlag -> flag.displayEnd
+        printFlag -> flag.prinfEnd
+        qFlag -> flag.question
+        commuEndFlag -> flag.commuEnd
+
+    *id -> id.*
+    *Point -> id.*
+    playingIdolNum -> id.playingIdol
+        choiceId -> id.choice
+        displayID -> id.display
+        commuPoint -> id.commu
+        textPoint -> id.text
+        playingIdolNum -> id.playingIdol
+*/
 
 $(function() {
     var reader;
     playingIdolNum = 0;
+    //Actually, playingIdolNum is defined when Player choose idol to produce
     //今回はテスト用コードなのでここは0になっていますが、実際の環境ではアイドル選択画面で選択した段階で値が変わります。
 
     function onChange(event) {
